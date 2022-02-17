ba39e9a1e48544d1b493f5460ff7565e59cdfeeb
Emirhan <81803760+xrenata@users.noreply.github.com>
2022-02-17 14:51:53 +0300

    added new command

---
+++ src/Commands/owner/eval.js
@@ -0,0 +1,31 @@
+const Command = require('../../Base/Command');
+const Discord = require('discord.js');
+module.exports = class extends Command {
+
+    constructor() {
+        super({
+            name: 'eval'
+        });
+    }
+    async run(ctx) {
+ if (ctx.message.author.id !== "382612768924368906")
+    return message.reply('you are not linux bot dev :pensive:');
+  try {
+    let codein = ctx.args.join(' ');
+    let code = eval(codein);
+    if (!codein) return;
+      code = eval(code);
+    if (typeof code !== 'string')
+      code = require('util').inspect(code, { depth: 0 });
+    let embed = new Discord.MessageEmbed()
+      .setTitle('Output')
+      .setDescription(`\`\`\`js\n${code}\n\`\`\``)
+    ctx.reply({embeds: [embed]});
+  } catch (e) {
+    let embed = new Discord.MessageEmbed()
+      .setTitle('Error')
+      .setDescription(`\`\`\`js\n${e}\n\`\`\``)
+    ctx.reply({embeds: [embed]});
+  }
+}
+}
