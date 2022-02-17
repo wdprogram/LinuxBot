fe671f8e02e669026a3fe128eea6408128fed05f
Emirhan <81803760+xrenata@users.noreply.github.com>
2022-02-17 18:25:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const Bot = require('./Base/Bot');
 require('dotenv').config();
 
-const bot = new Bot();
+const bot = new Bot();//bot
 
 bot.start(process.env.TOKEN).then(() => console.log('Bot is online'));
