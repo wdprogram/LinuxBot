f9175aad5366d30be7ff1df33df931a2ee42b673
Okeanos <76732468+Okeanos-0@users.noreply.github.com>
2022-02-19 19:16:44 +0300

    add: activity

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 const Bot = require('./Base/Bot');
 require('dotenv').config();
 
-const bot = new Bot();//bot
+const bot = new Bot(); //bot
 
-bot.start(process.env.TOKEN).then(() => console.log('Bot is online'));
+bot.start(process.env.TOKEN).then(() => {
+    console.log('Bot is ready');
+    bot.user.setPresence({
+        activities: [
+            {
+                name: 'sudo',
+                type: 0,
+            },
+        ],
+        status: 'dnd',
+    });
+});
